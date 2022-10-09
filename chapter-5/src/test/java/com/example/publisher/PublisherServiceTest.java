e4d841c9ede15effaa894dd8953756a594056c67
Josh McHugh <joshua.mchugh01@gmail.com>
2022-10-09 00:05:26 -0400

    Starting tests for Publisher Service

---
+++ chapter-5/src/test/java/com/example/publisher/PublisherServiceTest.java
@@ -0,0 +1,11 @@
+package com.example.publisher;
+
+import javax.transaction.Transactional;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+@Transactional
+public class PublisherServiceTest {
+        
+}
