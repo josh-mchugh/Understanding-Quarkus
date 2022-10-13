81441998d6b3140d9a91a3a21d05cba7d14ed40e
Josh McHugh <joshua.mchugh01@gmail.com>
2022-10-12 23:04:13 -0400

    Added GET Author endpoint and tests

--- chapter-6/src/main/java/com/example/AuthorResource.java
+++ chapter-6/src/main/java/com/example/AuthorResource.java
@@ -1,9 +1,13 @@
 package com.example;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -11,7 +15,7 @@ import javax.ws.rs.core.Response;
 @Path("/author")
 public class AuthorResource {
 
-    private final List<Author> authors = List.of(
+    private List<Author> authors = Arrays.asList(
         new Author().id(1).firstName("H.P.").lastName("Lovecraft"),
         new Author().id(2).firstName("August").lastName("Derleth"),
         new Author().id(3).firstName("Robert").lastName("Bloch"),
@@ -32,5 +36,19 @@ public class AuthorResource {
     public Response getAuthors() {
 
         return Response.ok(authors).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAuthor(@PathParam("id") Integer id) {
+
+        Optional<Author> authorOptional = authors.stream()
+            .filter(author -> author.getId().equals(id))
+            .findFirst();
+
+        return authorOptional.isPresent() 
+            ? Response.ok(authorOptional.get()).build()
+            : Response.noContent().build();
     }
 }
--- chapter-6/src/test/java/com/example/AuthorResourceTest.java
+++ chapter-6/src/test/java/com/example/AuthorResourceTest.java
@@ -2,10 +2,14 @@ package com.example;
 
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasKey;
 
 import javax.ws.rs.core.Response;
 
@@ -39,5 +43,43 @@ public class AuthorResourceTest {
           .get()
         .then()
           .body("$.size()", is(5));
+    }
+
+    @Test
+    public void whenGetAuthorByIdHasValidIdThenExpectOk() {
+      given()
+        .when()
+          .get("/{id}", 1)
+        .then()
+          .statusCode(Response.Status.OK.getStatusCode());
+    }
+
+    @Test
+    public void whenGetAuthorByIdHasInvalidIdThenExpectNoContent() {
+      given()
+        .when()
+          .get("/{id}", Integer.MIN_VALUE)
+        .then()
+          .statusCode(Response.Status.NO_CONTENT.getStatusCode());
+    }
+
+    @Test
+    public void whenGetAuthorByIdIsValidExpectJsonKeys() {
+      given()
+        .when()
+          .get("/{id}", 1)
+        .then()
+          .body("$", hasKey("first_name"))
+          .body("$", hasKey("last_name"));
+    }
+
+    @Test
+    public void whenGetAuthorByIdIsValidExpectValues() {
+      given()
+        .when()
+          .get("/{id}", 1)
+        .then()
+          .body("first_name", is("H.P."))
+          .body("last_name", is("Lovecraft"));
     }
 }
