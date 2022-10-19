29ac0c056a48a0412e5f275b4f79cbaefce71709
Josh McHugh <joshua.mchugh01@gmail.com>
2022-10-18 22:18:35 -0400

    Added fault tolerance to endpoint, pretty cool stuff

--- chapter-7/src/main/java/com/example/DadJokeResource.java
+++ chapter-7/src/main/java/com/example/DadJokeResource.java
@@ -5,6 +5,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @Path("/dad")
@@ -19,8 +22,21 @@ public class DadJokeResource {
     @GET
     @Path("/joke")
     @Produces(MediaType.TEXT_HTML)
-    public String getDadJoke() {
+    @Timeout(250)
+    @Fallback(fallbackMethod = "jokeFallback")
+    @CircuitBreaker(
+        requestVolumeThreshold = 4,
+        failureRatio = 0.5,
+        successThreshold = 2,
+        delay = 2000
+    )
+    public String getDadJoke() {        
 
         return jokeClient.getDadJoke().getJoke();
+    }
+
+    public String jokeFallback() {
+
+        return "A horse walks into a bar. The bar tender says\"Hey.\" The horse says \"Sure.\"";
     }
 }
--- chapter-7/src/main/java/com/example/JokeClient.java
+++ chapter-7/src/main/java/com/example/JokeClient.java
@@ -1,15 +1,12 @@
 package com.example;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.eclipse.microprofile.rest.client.annotation.ClientHeaderParam;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-@RegisterRestClient(baseUri = "https://icanhazdadjoke.com/")
+@RegisterRestClient(baseUri = "https://icanhazdadjoke24.com/")
 public interface JokeClient {
     
     @GET
