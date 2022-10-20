f825198182d32908a3e14be92299d11f19cfae02
Josh McHugh <joshua.mchugh01@gmail.com>
2022-10-19 21:33:29 -0400

    Refactored GreetingResource to use Mutiny

--- chapter-8/src/main/java/com/example/GreetingResource.java
+++ chapter-8/src/main/java/com/example/GreetingResource.java
@@ -1,16 +1,43 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+
+import io.smallrye.mutiny.Uni;
 
 @Path("/hello")
 public class GreetingResource {
 
+    private final List<String> GREETINGS = new ArrayList<>(List.of(
+        "Hello",
+        "hola",
+        "bonjour",
+        "guten tag",
+        "salve",
+        "nǐn hǎo",
+        "olá",
+        "asalaam alaikum",
+        "konnichiwa",
+        "anyoung haseyo",
+        "Zdravstvuyte"
+    ));
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    public Uni<String> getDefaultGreeting(@QueryParam("name") String name) {
+
+        return Uni.createFrom()
+            .item(formatGreeting("Hello", name));
+    }
+
+    private String formatGreeting(String greeting, String name) {
+
+        return String.format("%s %s", greeting, name);
     }
 }
