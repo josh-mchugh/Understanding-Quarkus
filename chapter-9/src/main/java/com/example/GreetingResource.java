41e6379557c8aa147fbd6d7024c018eaae9d774c
Josh McHugh <joshua.mchugh01@gmail.com>
2022-10-22 13:55:20 -0400

    Refactord HelloResource mertrics to used Time

--- chapter-9/src/main/java/com/example/GreetingResource.java
+++ chapter-9/src/main/java/com/example/GreetingResource.java
@@ -5,16 +5,18 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 
 @Path("/hello")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    @Counted(
-        name = "hello",
-        description = "Counts how many times hello resource endpoint has been invoked"
+    @Timed(
+        name = "timeHello",
+        description = "Times how long it tests to invloke the hello method",
+        unit = MetricUnits.MILLISECONDS
     )
     public String hello() {
         return "Hello from RESTEasy Reactive";
--- chapter-9/src/test/java/com/example/GreetingResourceTest.java
+++ chapter-9/src/test/java/com/example/GreetingResourceTest.java
@@ -3,14 +3,14 @@ package com.example;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasKey;
 
 import javax.ws.rs.core.Response;
-
-import org.hamcrest.CoreMatchers;
 
 @QuarkusTest
 public class GreetingResourceTest {
@@ -25,7 +25,7 @@ public class GreetingResourceTest {
     }
 
     @Test
-    public void testHelloEndpointCounted() {
+    public void testHelloEndpointMetrics() {
 
         given().when().get("/hello");
 
@@ -35,7 +35,22 @@ public class GreetingResourceTest {
                 .get("/q/metrics/application")
             .then()
                 .statusCode(Response.Status.OK.getStatusCode())
-                .body("'com.example.GreetingResource.hello'", is(1));
+                .body("'com.example.GreetingResource.timeHello'.count", is(1))
+                .body("'com.example.GreetingResource.timeHello'", hasKey("p99"))
+                .body("'com.example.GreetingResource.timeHello'", hasKey("min"))
+                .body("'com.example.GreetingResource.timeHello'", hasKey("max"))
+                .body("'com.example.GreetingResource.timeHello'", hasKey("mean"))
+                .body("'com.example.GreetingResource.timeHello'", hasKey("p50"))
+                .body("'com.example.GreetingResource.timeHello'", hasKey("p999"))
+                .body("'com.example.GreetingResource.timeHello'", hasKey("stddev"))
+                .body("'com.example.GreetingResource.timeHello'", hasKey("p95"))
+                .body("'com.example.GreetingResource.timeHello'", hasKey("p98"))
+                .body("'com.example.GreetingResource.timeHello'", hasKey("p75"))
+                .body("'com.example.GreetingResource.timeHello'", hasKey("fiveMinRate"))
+                .body("'com.example.GreetingResource.timeHello'", hasKey("fifteenMinRate"))
+                .body("'com.example.GreetingResource.timeHello'", hasKey("meanRate"))
+                .body("'com.example.GreetingResource.timeHello'", hasKey("oneMinRate"))
+                .body("'com.example.GreetingResource.timeHello'", hasKey("elapsedTime"));
     }
 
 }
